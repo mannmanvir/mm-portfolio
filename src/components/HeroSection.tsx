3787d4b5c4799d30c94fcb59ead9ed95e1cb8760
Manvir Mann <72842990+mannmanvir@users.noreply.github.com>
2024-06-28 21:03:40 -0700

    * Fixed headshot to be a circular shape

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,13 +10,14 @@ const HeroSection = () => {
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-          <Image
-            src="/headshot.png"
-            alt=""
-            width={325}
-            height={325}
-            className="rounded-full shadow-2xl"
-          />
+          <div className="w-80 h-80 relative">
+            <Image
+              src="/headshot.png"
+              alt="Headshot"
+              fill
+              className="rounded-full object-cover object-top shadow-2xl"
+            />
+          </div>
         </div>
         <div className="md:mt-2 md:w-3/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">
